561fb0210bbcd287502b4850bcc4c9811e2def96
Samad Rathod <samadrathod87@gmail.com>
2026-02-14 21:47:10 +0530

    login attened

--- js/auth.js
+++ js/auth.js
@@ -3,9 +3,18 @@ import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-const from=
+const form=
 document.getElementById("authform");
-from.addEventListener("submit",(e) =>{
-    e.preventDefault();
-    console.log("Form Submitted");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    console.log("Account created");
+    window.location.href = "index.html";
+  } catch (err) {
+    console.log(err.message);
+  }
 });
