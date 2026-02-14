624d3ee77c42592a0bcc81fd9551c3c58a57e5aa
Samad Rathod <samadrathod87@gmail.com>
2026-02-14 20:13:31 +0530

    fixed error

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@ import { auth } from "./firebase.js"
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-js";
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 const from=
 document.getElementById("authform");
 from.addEventListener("submit",(e) =>{
