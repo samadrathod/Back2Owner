4c12f2260e55f2a99bfb86402d77e63ae7ad6ea6
Samad Rathod <samadrathod87@gmail.com>
2026-03-18 10:21:28 +0530

    Fixed ui a little

--- js/addItem.js
+++ js/addItem.js
@@ -3,7 +3,7 @@ import { onAuthStateChanged } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { collection,addDoc } from 
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-//page protect rideirect
+//page protect redirect
 onAuthStateChanged(auth,(user) =>{
     if(!user) {
         window.location.href="login.html";
